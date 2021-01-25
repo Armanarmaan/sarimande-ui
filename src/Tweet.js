a3c95e0b3573fee2963154ecdc7a331995511e6c
La Ode Arman <arman.41@hotmail.com>
2021-01-25 23:33:39 +0700

    2nd commit

---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function tweet(){
+  return (
+    <div className="tweet">
+      <h3>Name</h3>
+      <p>This is a random tweet</p>
+      <h3>Likes</h3>
+    </div>
+  );
+}
+
+export default tweet;
