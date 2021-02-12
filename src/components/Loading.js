2f13b86598a0dcd4bb51fc52b712b798a6b83351
La Ode Arman <arman.41@hotmail.com>
2021-02-12 20:04:45 +0700

    loading screen done

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -9,7 +9,8 @@ function Loading() {
   const imgLoad2 = require('../assets/img-load-2.png').default;
   const imgLoad3 = require('../assets/img-load-3.png').default;
   const imgLoad4 = require('../assets/img-load-4.png').default;
-  const imgLoad5 = require('../assets/img-load-home.png').default;
+  // const imgLoad5 = require('../assets/img-load-home-colored.png').default;
+  const imgLoad5 = require('../assets/fullscreen-example.jpg').default;
 
   const [imgLoading, setImgLoading] = useState(0);
 
@@ -19,45 +20,50 @@ function Loading() {
     setTimeout(function(){ 
       setImgLoading(imgLoad1); 
       changeImage2();
-    }, 700);
+    }, 500);
   }, []);
 
   const changeImage2 = () =>{
     setImgLoading(imgLoad2);
     setTimeout(function(){ 
       changeImage3();
-    }, 700);
+    }, 500);
   };
   const changeImage3 = () =>{
     setImgLoading(imgLoad3);
     setTimeout(function(){ 
       changeImage4();
-    }, 700);
+    }, 500);
   };
   const changeImage4 = () =>{
     setImgLoading(imgLoad4);
     setTimeout(function(){ 
       changeImage5();
-    }, 700);
+    }, 500);
   };
   const changeImage5 = () =>{
     setImgLoading(imgLoad5);
     setTimeout(function(){ 
       changeBorderSize();
-    }, 700);
+    }, 500);
   };
   const changeBorderSize = () =>{
     $('.img-border').addClass('go');
     $('.loading-images').addClass('goDown');
+    $('.logo').addClass('goUp');
     setTimeout(function(){ 
       hideLoading();
-    }, 2000);
+    }, 2700);
   };
   const hideLoading = () =>{
     $('.loading-container').removeClass('d-flex');
     $('.loading-container').addClass('d-none');
+    $('body').removeClass('modal-open');
   };
 
+  $('body').addClass('modal-open');
+  window.scrollTo(0, 0);
+  
   return (
     <div className="loading-container d-flex justify-content-center">
       <div className="img-container">
