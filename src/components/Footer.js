d731ab33f01410b8fce6e714060bfeb4beff72bf
lanoharahap <harahaplano@yahoo.co.id>
2021-02-21 20:29:56 +0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,75 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>
+    <footer className="footer container">
       <div>
         <p>Logo</p>
       </div>
-      <div>
-        <p>SiteMap</p>
-        <a>FoodMenu</a>
-        <a>Order</a>
-        <a>Gallery</a>
+      <div className="items">
+        <div>
+          <p>SiteMap</p>
+          <ul className="itemlist">
+            <li>
+              <a>FoodMenu</a>
+            </li>
+            <li>
+              <a>Order</a>
+            </li>
+            <li>
+              <a>Gallery</a>
+            </li>
+          </ul>
+
+        </div>
+        <div>
+          <p>Location</p>
+          <ul className="itemlist">
+            <li>
+              <a>Menteng</a>
+            </li>
+            <li>
+              <a>Sabang</a>
+            </li>
+            <li>
+              <a>Pancoran</a>
+            </li>
+            <li>
+              <a>lebak Bulus</a>
+            </li>
+            <li>
+              <a>Cibubur</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <p>Follow US</p>
+          <ul className="medsos">
+            <li>
+              <a>ig</a>
+            </li>
+            <li>
+              <a>fb</a>
+            </li>
+          </ul>
+          <p>Be in the Know</p>
+          <p>Subscribe to Sarimande Metropolitan newsletter</p>
+          <input type="email" placeholder="Email" />
+          <button type="button">Sign Up Now</button>
+        </div>
       </div>
-    </div>
+      <div className="bot">
+        <div className="terms">
+          <a>Term of Use</a>
+          <a>Privacy Statement</a>
+        </div>
+
+        <div className="copyright">
+          <a>@2021.Clay Web Development.</a>
+        </div>
+
+      </div>
+    </footer>
 
   );
 }
