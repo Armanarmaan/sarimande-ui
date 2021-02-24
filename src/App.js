320b65073e77708e3c9c1bc4c6b4ad10a554efbe
La Ode Arman <arman.41@hotmail.com>
2021-02-24 19:58:38 +0700

    saving notfound

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Shop from './components/Shop';
 import Loading from './components/Loading';
+import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
           <Route path="/shop" exact component={Shop} />
+          <Route component={NotFound} />
         </Switch>
         <Footer />
       </div>
