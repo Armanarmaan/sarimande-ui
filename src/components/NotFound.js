419468ce3b83bb8938fe6cd44f7ee19f91c5a5ac
La Ode Arman <arman.41@hotmail.com>
2021-02-25 21:49:11 +0700

    add notfound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,20 @@
 import '../style/NotFound.scss';
 import $ from "jquery";
+import { useHistory } from 'react-router-dom';
 
 function NotFound() {
   setTimeout(function(){ 
     $('#modalNotFound').modal('show');
   }, 100);
+
+  const history = useHistory();
+
+  function goToHome() {
+    console.log("jalan ngab");
+    $('#modalNotFound').modal('hide');
+    history.push('/');
+  }
+
   return (
     <div className="container-not-found"> 
       <div className="modal fade" id="modalNotFound" tabindex="-1" role="dialog">
@@ -12,8 +22,12 @@ function NotFound() {
           <div className="modal-content">
             <div className="background-shadow centered"></div>
             <div className="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+              <button type="button" className="close" onClick={goToHome}>
+                <span aria-hidden="true">
+                  <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.666 1.66666L1.66602 17.6667M17.666 17.6667L1.66602 1.66666L17.666 17.6667Z" stroke="white" strokeWidth="2" stroke-linecap="round"/>
+                  </svg>
+                </span>
               </button>
             </div>
             <div className="modal-body">
